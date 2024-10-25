63b97e1200ba92681a8035488919fe0e432d18ab
henrynguci <henrynguci@gmail.com>
2024-10-25 10:23:22 +0700

    fix crash - add colors

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import colors from 'colors';
 
 dotenv.config();
 
@@ -26,10 +27,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-            .bold,
-    );
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
 app.use((req, res) => {
